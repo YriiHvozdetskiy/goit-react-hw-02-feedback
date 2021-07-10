e8245ed5941ccf39c1690a2033a3c98f1bf0ef10
YriiHvozdetskiy <hvozdetskiyyrii@gmail.com>
2021-07-10 09:20:21 +0300

    Updates

---
+++ static/js/main.e9adb692.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonphw-02-feedback"]=this["webpackJsonphw-02-feedback"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),d=c.n(s),i=c(0),o=function(){return Object(i.jsx)("div",{})};d.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.e9adb692.chunk.js.map
